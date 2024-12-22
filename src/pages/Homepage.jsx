19726569baf982c5976021b928bc20a8a82f833b
adnan <sirgeant10@gmail.com>
2024-12-22 18:27:52 +0600

    Show Service Categories

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
+import ServiceCategories from "../components/ServiceCategories";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <ServiceCategories />
     </>
   );
 };
