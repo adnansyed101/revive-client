819d9f4b8ca21e8372e56d0ea6cb054e7a3509c6
adnan <sirgeant10@gmail.com>
2024-12-22 19:00:59 +0600

    Add signin route

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import SignIn from "../pages/SignIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
   },
 ]);
 
