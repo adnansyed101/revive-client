a3fe02a4eca7d164159cac379130fdc45ab2796d
adnan <sirgeant10@gmail.com>
2024-12-22 18:11:06 +0600

    Show banner comp

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
+import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
-      <h1>Homepage</h1>
+      <Banner />
     </>
   );
 };
