796a2330d24d324b7897c1d62ae17c3bf017c804
adnan <sirgeant10@gmail.com>
2024-12-22 20:44:23 +0600

    Add scroll and title.
    The scroll to will go to the top of the page. The title will change the
    title of the page.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -5,6 +6,11 @@ import PopularServices from "../components/PopularServices";
 import ServiceCategories from "../components/ServiceCategories";
 
 const Homepage = () => {
+  useEffect(() => {
+    document.title = "R&R | Home";
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Navbar />
