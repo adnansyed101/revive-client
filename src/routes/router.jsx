f1664dc3b1b60e94ff750c1642af729742499a8c
adnan <sirgeant10@gmail.com>
2024-12-23 14:04:36 +0600

    Add route to add service

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import CreateServices from "../pages/CreateServices";
+import PrivateRoute from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignUp />,
+  },
+  {
+    path: "/addService",
+    element: (
+      <PrivateRoute>
+        <CreateServices />,
+      </PrivateRoute>
+    ),
   },
 ]);
 
