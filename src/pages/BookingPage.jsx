27efc25b6ee1bb86761245b2a92611f39e953a1a
adnan <sirgeant10@gmail.com>
2024-12-25 15:24:50 +0600

    Create booking Page

---
+++ src/pages/BookingPage.jsx
@@ -0,0 +1,15 @@
+import Booking from "../components/Booking";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const BookingPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Booking />
+      <Footer />
+    </>
+  );
+};
+
+export default BookingPage;
