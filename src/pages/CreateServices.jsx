a9b9e7276c6934a29be4f320d62cba1cede2e5c7
adnan <sirgeant10@gmail.com>
2024-12-25 12:57:59 +0600

    Create a service on form submit

--- src/pages/CreateServices.jsx
+++ src/pages/CreateServices.jsx
@@ -1,25 +1,63 @@
+import axios from "axios";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Button from "../components/UI/Button";
+import useAuth from "../hooks/useAuth";
 
 const CreateServices = () => {
+  const { user } = useAuth();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const imgURL = form.imgURL.value;
+    const serviceName = form.serviceName.value;
+    const price = form.price.value;
+    const serviceArea = form.serviceArea.value;
+    const description = form.description.value;
+
+    const newService = {
+      imgURL,
+      serviceName,
+      price,
+      serviceArea,
+      description,
+      provider: {
+        imgURL: user.photoURL,
+        name: user.displayName,
+        email: user.email,
+      },
+    };
+
+    axios
+      .post(
+        `${import.meta.env.VITE_SERVERURL}/api/service/add-service`,
+        newService
+      )
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <Navbar />
       <div className="flex items-center justify-center min-h-[calc(100vh-50px)] mt-20 mb-10">
         <div className="w-full max-w-md p-8 space-y-6 rounded-lg shadow-md bg-base-200">
           <h2 className="text-2xl font-bold text-center">Add a New Service</h2>
-          <form className="space-y-4 ">
+          <form className="space-y-4" onSubmit={handleSubmit}>
             <div>
               <label
-                htmlFor="imageURL"
+                htmlFor="imgURL"
                 className="block text-sm font-medium mb-2"
               >
                 Image URL of the Service
               </label>
               <input
                 type="url"
-                id="imageURL"
+                id="imgURL"
+                name="imgURL"
                 placeholder="Enter the image URL"
                 className="input input-bordered input-primary w-full"
               />
@@ -34,6 +72,7 @@ const CreateServices = () => {
               <input
                 type="text"
                 id="serviceName"
+                name="serviceName"
                 placeholder="Enter the service name"
                 className="input input-bordered input-primary w-full"
               />
@@ -45,6 +84,7 @@ const CreateServices = () => {
               <input
                 type="number"
                 id="price"
+                name="price"
                 placeholder="Enter the price"
                 className="input input-bordered input-primary w-full"
               />
@@ -59,6 +99,7 @@ const CreateServices = () => {
               <input
                 type="text"
                 id="serviceArea"
+                name="serviceArea"
                 placeholder="Enter the service area"
                 className="input input-bordered input-primary w-full"
               />
@@ -72,6 +113,7 @@ const CreateServices = () => {
               </label>
               <textarea
                 id="description"
+                name="description"
                 placeholder="Enter the service description"
                 className="textarea textarea-primary w-full"
                 rows="4"
