7b713ee2ed8210db488db44f61a1781091e88eb9
adnan <sirgeant10@gmail.com>
2024-12-25 22:11:17 +0600

    Show cards else show services text

--- src/pages/ManageService.jsx
+++ src/pages/ManageService.jsx
@@ -61,40 +61,44 @@ const ManageService = () => {
         <div className="max-w-5xl w-full bg-base-200 shadow-lg rounded-lg p-6">
           <h2 className="text-2xl font-bold mb-6">Manage Services</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {services.map((service) => (
-              <div
-                key={service._id}
-                className="card bg-base-100 shadow-md border border-gray-200 rounded-lg p-4"
-              >
-                <figure className="mb-4">
-                  <img
-                    src={service.imgURL}
-                    alt={service.serviceName}
-                    className="w-full h-40 object-cover rounded-md"
-                  />
-                </figure>
-                <h3 className="text-lg font-bold  mb-2">
-                  {service.serviceName}
-                </h3>
-                <p className=" mb-2">Price: ${service.price}</p>
-                <p className=" mb-4">Area: {service.serviceArea}</p>
-                <div className="flex space-x-4">
-                  <Link to={`/services/update/${service._id}`}>
-                    <Button className="btn btn-outline btn-sm btn-primary">
-                      <FaEdit />
-                      <span>Update</span>
+            {services.length > 0 ? (
+              services.map((service) => (
+                <div
+                  key={service._id}
+                  className="card bg-base-100 shadow-md border border-gray-200 rounded-lg p-4"
+                >
+                  <figure className="mb-4">
+                    <img
+                      src={service.imgURL}
+                      alt={service.serviceName}
+                      className="w-full h-40 object-cover rounded-md"
+                    />
+                  </figure>
+                  <h3 className="text-lg font-bold  mb-2">
+                    {service.serviceName}
+                  </h3>
+                  <p className=" mb-2">Price: ${service.price}</p>
+                  <p className=" mb-4">Area: {service.serviceArea}</p>
+                  <div className="flex space-x-4">
+                    <Link to={`/services/update/${service._id}`}>
+                      <Button className="btn btn-outline btn-sm btn-primary">
+                        <FaEdit />
+                        <span>Update</span>
+                      </Button>
+                    </Link>
+                    <Button
+                      className="btn btn-outline btn-sm btn-error"
+                      onClick={() => showConfirmationToast(service._id)}
+                    >
+                      <FaTrash />
+                      <span>Delete</span>
                     </Button>
-                  </Link>
-                  <Button
-                    className="btn btn-outline btn-sm btn-error"
-                    onClick={() => showConfirmationToast(service._id)}
-                  >
-                    <FaTrash />
-                    <span>Delete</span>
-                  </Button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))
+            ) : (
+              <h1>No Services Created</h1>
+            )}
           </div>
         </div>
       </div>
