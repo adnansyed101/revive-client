97ca2a180631c2d26a614aaa11f777f1eebce6cd
Adnan <sirgeant10@gmail.com>
2025-02-28 17:19:12 +0600

    Move components to shared folder.

--- src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="loading loading-spinner text-accent"></span>
-      </div>
-    );
-  };
-  
-  export default Loading;
