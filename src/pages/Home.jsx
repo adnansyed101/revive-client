5bb20d7a4e662f3826eeb721b6adf5cbd718bedd
Adnan <sirgeant10@gmail.com>
2025-02-28 17:25:17 +0600

    Fix import.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import Banner from "../components/Banner";
-import PopularServices from "../components/PopularServices";
-import ServiceCategories from "../components/ServiceCategories";
-import RepairProcess from "../components/RepairProcess";
-import WebsiteInsights from "../components/WebsiteInsights";
+import Banner from "../components//Home/Banner";
+import PopularServices from "../components/Home/PopularServices";
+import ServiceCategories from "../components/Home/ServiceCategories";
+import RepairProcess from "../components/Home/RepairProcess";
+import WebsiteInsights from "../components/Home/WebsiteInsights";
 
 const Home = () => {
   useEffect(() => {
